0b9d70775f317cedc4fdb4515092bf1f255170fe
Gabriel <gaboelnuevo@gmail.com>
2017-07-30 01:40:23 -0600

    search screen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -4,18 +4,30 @@ import {
   Platform,
   ScrollView,
   StyleSheet,
-  Text,
   TouchableOpacity,
   View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
+import { Container, Content, Card, CardItem, Text, Icon, Button, CheckBox } from 'native-base';
+
 import { MonoText } from '../components/StyledText';
+
+import Slider from 'react-native-slider';
+
+import Stars from 'react-native-stars';
 
 export default class SearchScreen extends React.Component {
   static navigationOptions = {
-    header: null,
+    title: "Search"
   };
+
+  constructor(props){
+    super(props);
+    this.state = {
+      price: 1
+    }
+  }
 
   render() {
     return (
@@ -23,98 +35,50 @@ export default class SearchScreen extends React.Component {
         <ScrollView
           style={styles.container}
           contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View
-              style={[
-                styles.codeHighlightContainer,
-                styles.homeScreenFilename,
-              ]}>
-              <MonoText style={styles.codeHighlightText}>
-                screens/HomeScreen.js
-              </MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity
-              onPress={this._handleHelpPress}
-              style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>
-                Help, it didn’t automatically reload!
-              </Text>
-            </TouchableOpacity>
-          </View>
+              <Container>
+                <Content>
+                    <Card>
+                        <CardItem>              
+                            <Icon name="calculator" style={{ color: '#DD5044' }} />
+                            <Text style={{flex: 1}}>Price</Text>
+                            <CheckBox checked={true} />
+                        </CardItem>
+                        <CardItem>
+                          <View style={{flex: 1}}>
+                            <Slider
+                              value={this.state.value}
+                              onValueChange={(value) => this.setState({value})} />
+                          </View>              
+                        </CardItem>
+                        <CardItem>              
+                            <Icon name="star-half" style={{ color: '#DD5044' }} />
+                            <Text style={{flex: 1}}>Ranking</Text>
+                            <CheckBox checked={true} />
+                        </CardItem>
+                        <CardItem>
+                          <View style={{flex: 1}}>
+                          <Stars
+                            half={true}
+                            rating={2.5}
+                            update={(val)=>{this.setState({stars: val})}}
+                            spacing={4}
+                            starSize={40}
+                            count={5}
+                            fullStar={require('../node_modules/react-native-stars/example-images/starFilled.png')}
+                            emptyStar={require('../node_modules/react-native-stars/example-images/starEmpty.png')}
+                            halfStar={require('../node_modules/react-native-stars/example-images/starHalf.png')}/>
+                          </View>                  
+                        </CardItem>
+                   </Card>
+                   <Button full>
+                     <Text>Search</Text>
+                   </Button>
+                </Content>
+            </Container>
         </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>
-            This is a tab bar. You can edit it in:
-          </Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>
-              navigation/MainTabNavigator.js
-            </MonoText>
-          </View>
-        </View>
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use
-          useful development tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/development-mode'
-    );
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
@@ -122,86 +86,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
   contentContainer: {
-    paddingTop: 30,
+    paddingTop: 5,
+    paddingVertical: 4,
+    paddingHorizontal: 6
   },
-  welcomeContainer: {
+  startsContainer: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  }
 });
